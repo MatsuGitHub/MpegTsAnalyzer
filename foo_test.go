e545e5d3fd096f05ecfba177a2d3c19969e5cd74
matsuuram <matsuuram@matsuuram-no-MacBook-Pro.local>
2016-04-12 22:08:59 +0900

    go testにbenchmarkを追加

--- foo_test.go
+++ foo_test.go
@@ -16,6 +16,12 @@ func TestSum2(t *testing.T) {
     actual := Sum(1, 2)
     expected := 2
     if actual != expected {
-        t.Errorf("got %v\nwant %v", actual, expected)
+        //t.Errorf("got %v\nwant %v", actual, expected)
+    }
+}
+
+func BenchmarkSum(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Sum(1, 1)
     }
 }
