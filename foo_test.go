c659453764a156bea08816c61204e610fa3dcc18
matsuuram <matsuuram@matsuuram-no-MacBook-Pro.local>
2016-04-12 21:52:58 +0900

    fix import define.

--- foo_test.go
+++ foo_test.go
@@ -2,11 +2,10 @@ package foo
 
  import (
      "testing"
-     "github.com/MatsuGitHub/test"
  )
 
 func TestSum(t *testing.T) {
-    actual := foo.Sum(10, 20)
+    actual := Sum(10, 20)
     expected := 30
     if actual != expected {
         t.Errorf("got %v\nwant %v", actual, expected)
@@ -14,7 +13,7 @@ func TestSum(t *testing.T) {
 }
 
 func TestSum2(t *testing.T) {
-    actual := foo.Sum(1, 2)
+    actual := Sum(1, 2)
     expected := 3
     if actual != expected {
         t.Errorf("got %v\nwant %v", actual, expected)
